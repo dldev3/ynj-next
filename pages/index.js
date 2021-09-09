ac295a3c1367ef5499c60d23bd63c773dbd88b9b
dlshnkLK <dilshankaliyanage@yahoo.com>
2021-09-09 17:49:42 +0530

    images added

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,13 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>YNJ Immigration</title>
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-purple-400">Hello</h1>
+        <h1 className="text-purple-400">Hello YNJ</h1>
+
       </main>
 
 
